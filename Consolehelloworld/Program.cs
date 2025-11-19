42cc46d4803d4bcda02612127ebc5f1a184f6b57
Naruesorn wichit 115 <Nakoss4399@gmail.com>
2025-11-19 10:21:31 +0700

    Update Program.cs

--- Consolehelloworld/Program.cs
+++ Consolehelloworld/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Consolehelloworld
+namespace Consolehelloworld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string name = "";
+            int height = 0;
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.Write("Please, input you full name (eng) :");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello, World!" + name);
+
+            Console.WriteLine("please fill in you height(cm) :");
+            height = Convert.ToInt16(Console.ReadLine());
+            int weight = height - 100;
+
+            Console.WriteLine("You ideal weight is " + weight.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("press any key to end program");
         }
     }
 }
